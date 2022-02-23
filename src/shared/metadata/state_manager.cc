k8sstormcenter/pixie

commit a4139ecd3737622fd1b6b655e58a7610331cbd3c

NetworkStatsConnector Bug-fix: More consistent stats when container PIDs switch namespaces
Summary:
In the rare case when a container process switches namespaces with a call to setns,
NetworkStatsConnector can incorrectly produce stats from the wrong namespace.

Mitigate this effect by choosing the oldest PID in the container. This provides stability in which PID is chosen for producing stats.

We also assume here that the oldest PID is not the one switching namespaces, so a more robust fix may be required in the future.

Note that this bug was found by monitoring the PEM itself. Stirling forks a Java agent attach process which then changes namespaces,
causing mis-reported network stats.

Test Plan: Test added for the ordering. Also checked manually that the mis-reported network stats are fixed.

Reviewers: #stirling, michelle, jps

Reviewed By: #stirling, jps

Signed-off-by: Omid Azizi <oazizi@pixielabs.ai>

Differential Revision: https://phab.corp.pixielabs.ai/D10823

GitOrigin-RevId: 021a2d1d77805a7713a4e9a2acfae35c57a14255

src/shared/metadata/state_manager.cc
@@ -173,7 +173,7 @@ StatusOr<UPID> InitUPID(const system::ProcParser& proc_parser, uint32_t asid, ui
 
 void ProcessContainerPIDUpdates(
     CIDView cid, int64_t ts, const system::ProcParser& proc_parser, AgentMetadataState* md,
-    absl::flat_hash_set<UPID>* upids, absl::flat_hash_set<uint32_t>* cgroups_pids,
+    StartTimeOrderedUPIDSet* upids, absl::flat_hash_set<uint32_t>* cgroups_pids,
     moodycamel::BlockingConcurrentQueue<std::unique_ptr<PIDStatusEvent>>* pid_updates) {
   // Iterate through old list of UPIDs, looking for PIDs which have been deleted.
   auto upids_iter = upids->begin();

src/shared/metadata/k8s_objects.h
@@ -23,6 +23,7 @@
 #include <utility>
 #include <vector>
 
+#include <absl/container/btree_set.h>
 #include <absl/container/flat_hash_map.h>
 #include <absl/container/flat_hash_set.h>
 #include "src/common/base/base.h"
@@ -335,6 +336,20 @@ class PodInfo : public K8sMetadataObject {
   std::string pod_ip_;
 };
 
+struct UPIDStartTSCompare {
+  bool operator()(const UPID& a, const UPID& b) const {
+    if (a.start_ts() != b.start_ts()) {
+      return a.start_ts() < b.start_ts();
+    }
+    if (a.asid() != b.asid()) {
+      return a.asid() < b.asid();
+    }
+    return a.pid() < b.pid();
+  }
+};
+
+using StartTimeOrderedUPIDSet = absl::btree_set<UPID, UPIDStartTSCompare>;
+
 /**
  * Store information about containers.
  *
@@ -371,8 +386,8 @@ class ContainerInfo {
   void set_pod_id(std::string_view pod_id) { pod_id_ = pod_id; }
   const UID& pod_id() const { return pod_id_; }
 
-  const absl::flat_hash_set<UPID>& active_upids() const { return active_upids_; }
-  absl::flat_hash_set<UPID>* mutable_active_upids() { return &active_upids_; }
+  const StartTimeOrderedUPIDSet& active_upids() const { return active_upids_; }
+  StartTimeOrderedUPIDSet* mutable_active_upids() { return &active_upids_; }
 
   int64_t start_time_ns() const { return start_time_ns_; }
 
@@ -406,7 +421,7 @@ class ContainerInfo {
   /**
    * The set of UPIDs that are running on this container.
    */
-  absl::flat_hash_set<UPID> active_upids_;
+  StartTimeOrderedUPIDSet active_upids_;
 
   /**
    * Current state of the container, such as RUNNING, WAITING.

src/shared/metadata/k8s_objects_test.cc
@@ -127,6 +127,29 @@ TEST(ContainerInfo, debug_string) {
   EXPECT_EQ("<Container:cid=container1:name=containername:pod_id=:state=S>", cinfo.DebugString());
 }
 
+TEST(ContainerInfo, pids_are_time_sorted) {
+  ContainerInfo orig("container1", "containername", ContainerState::kRunning,
+                     ContainerType::kDocker, "container state message", "container state reason",
+                     128 /*start_time*/);
+  orig.set_pod_id("pod1");
+
+  auto* upids = orig.mutable_active_upids();
+  upids->emplace(UPID(1, 0, 200));
+  upids->emplace(UPID(1, 1, 100));
+  upids->emplace(UPID(1, 15, 400));
+  upids->emplace(UPID(1, 15, 300));
+  upids->emplace(UPID(1, 10, 300));
+  upids->emplace(UPID(0, 1, 500));
+  upids->emplace(UPID(0, 2, 300));
+  upids->emplace(UPID(0, 3, 100));
+
+  // UPIDs should be ordered by time.
+  EXPECT_THAT(
+      orig.active_upids(),
+      testing::ElementsAre(UPID(0, 3, 100), UPID(1, 1, 100), UPID(1, 0, 200), UPID(0, 2, 300),
+                           UPID(1, 10, 300), UPID(1, 15, 300), UPID(1, 15, 400), UPID(0, 1, 500)));
+}
+
 TEST(ContainerInfo, clone) {
   ContainerInfo orig("container1", "containername", ContainerState::kRunning,
                      ContainerType::kDocker, "container state message", "container state reason",
@@ -134,9 +157,9 @@ TEST(ContainerInfo, clone) {
   orig.set_pod_id("pod1");
 
   auto* upids = orig.mutable_active_upids();
-  upids->emplace(UPID(1, 0, 123));
-  upids->emplace(UPID(1, 1, 123));
-  upids->emplace(UPID(1, 15, 123));
+  upids->emplace(UPID(1, 0, 200));
+  upids->emplace(UPID(1, 1, 100));
+  upids->emplace(UPID(1, 15, 300));
 
   auto cloned = orig.Clone();
 

src/stirling/source_connectors/network_stats/network_stats_connector.cc
@@ -109,6 +109,15 @@ Status NetworkStatsConnector::GetNetworkStatsForPod(const system::ProcParser& pr
       continue;
     }
 
+    // We expect the UPIDs to be start-time ordered.
+    // This is so we prioritize picking the oldest UPID to get the network
+    // stats. Normally, any PID will do, since they are all in the same network namespace. However,
+    // there can be issues if any of the processes switch namespaces. This happens with Stirling's
+    // own Java Attacher, which can cause network stats to suddenly switch to the stats of a
+    // different namespace, producing incorrect results.
+    CHECK(std::is_sorted(container_info->active_upids().begin(),
+                         container_info->active_upids().end(), md::UPIDStartTSCompare()));
+
     for (const auto& upid : container_info->active_upids()) {
       auto s = proc_parser.ParseProcPIDNetDev(upid.pid(), stats);
       if (s.ok()) {

commit 1a8463293dee570d79fa28ebc5fb7b29f963addc

Fix seg-fault in upids set
Summary: Fix a seg-fault introduced in D10823.

Test Plan: Manually deployed on to a cluster and left it running for a while. Previously, the seg-fault would trigger within minutes. This no longer happens.

Reviewers: #stirling, michelle, yzhao

Reviewed By: #stirling, yzhao

Subscribers: yzhao

Signed-off-by: Omid Azizi <oazizi@pixielabs.ai>

Differential Revision: https://phab.corp.pixielabs.ai/D10836

GitOrigin-RevId: ef3b3a4d444afa7778963fef889fbe2b09604426

src/shared/metadata/state_manager.cc
@@ -188,7 +188,9 @@ void ProcessContainerPIDUpdates(
       // Push deletion events to the queue.
       pid_updates->enqueue(std::make_unique<PIDTerminatedEvent>(prev_upid, ts));
 
-      upids->erase(upids_iter++);
+      // Must use this style instead of `upids->erase(upids_iter++)`,
+      // otherwise the iterator becomes invalid, causing potential seg-faults.
+      upids_iter = upids->erase(upids_iter);
       continue;
     }
 
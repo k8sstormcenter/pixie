k8sstormcenter/pixie

commit 451a26b34bc68a1e55cb4c83c4906fc78111be27

Jenkins job trigger fixes
Summary:
Tests did not run properly in https://phab.corp.pixielabs.ai/D6919?id=24800.

While the bazel query producing `bazel_tests_bpf`  was properly identifying the test to run, it was being gated by `bazel_cc_changed` which didn't realize that a shell test was involved.

As a result, made a few edits to decouple the bazel queries for the test targets from the cc check. Relatedly, also made the rule for the creation of docs more explicit, and decoupled from clang-tidy.

Not sure if everything here is 100% right, so please let me know if there was some intention behind some of the previous structure.

Test Plan: Jenkins/manual

Reviewers: #engineering, zasgar, vihang

Reviewed By: #engineering, vihang

Differential Revision: https://phab.corp.pixielabs.ai/D6922

GitOrigin-RevId: a170b1f18ff47e88f3369474bc36c5b443cd626a

Jenkinsfile
@@ -536,12 +536,6 @@ def checkoutAndInitialize() {
   }
 }
 
-def enableForCC(Closure body) {
-  if (shFileExists('bazel_cc_changed')) {
-    body()
-  }
-}
-
 def enableForTargets(String targetName, Closure body) {
   if (!shFileEmpty("bazel_buildables_${targetName}") || !shFileEmpty("bazel_tests_${targetName}")) {
     body()
@@ -672,30 +666,31 @@ if (runBPFWithTSAN) {
 
 def generateTestTargets = {
   enableForTargets('clang_opt') {
-      builders['Build & Test (clang:opt + UI)'] = buildAndTestOptWithUI
+    builders['Build & Test (clang:opt + UI)'] = buildAndTestOptWithUI
   }
 
-  enableForCC {
+  enableForTargets('clang_tidy') {
     builders['Clang-Tidy'] = buildClangTidy
+  }
 
-    enableForTargets('clang_dbg') {
-      builders['Build & Test (dbg)'] = buildDbg
-    }
+  enableForTargets('clang_dbg') {
+    builders['Build & Test (dbg)'] = buildDbg
+  }
 
-    enableForTargets('sanitizer') {
-      builders['Build & Test (asan)'] = buildASAN
-    }
+  enableForTargets('sanitizer') {
+    builders['Build & Test (asan)'] = buildASAN
+  }
 
-    enableForTargets('sanitizer') {
-      builders['Build & Test (tsan)'] = buildTSAN
-    }
-    enableForTargets('gcc_opt') {
-      builders['Build & Test (gcc:opt)'] = buildGCC
-    }
+  enableForTargets('sanitizer') {
+    builders['Build & Test (tsan)'] = buildTSAN
+  }
 
-    enableForTargets('bpf') {
-      builders['Build & Test (bpf tests - opt)'] = buildAndTestBPFOpt
-    }
+  enableForTargets('gcc_opt') {
+    builders['Build & Test (gcc:opt)'] = buildGCC
+  }
+
+  enableForTargets('bpf') {
+    builders['Build & Test (bpf tests - opt)'] = buildAndTestBPFOpt
   }
 }
 
@@ -756,7 +751,7 @@ builders['Lint & Docs'] = {
       sh 'arc lint --trace'
     }
 
-    enableForCC {
+    if (shFileExists('run_doxygen')) {
       def stashName = 'doxygen-docs'
       container('pxbuild') {
         sh 'doxygen'

ci/bazel_build_deps.sh
@@ -137,9 +137,12 @@ ${bazel_query} "kind(cc_test, attr('tags', 'requires_bpf', ${tests_kind})) \
   ${sanitizer_only}" > bazel_tests_bpf_sanitizer
 
 # Should we run clang-tidy?
-${bazel_query} "kind(cc_binary, ${buildables_kind})" > bazel_buildables_cc
-${bazel_query} "kind(cc_test, ${tests_kind})" > bazel_tests_cc
+${bazel_query} "kind(cc_binary, ${buildables_kind})" > bazel_buildables_clang_tidy
+${bazel_query} "kind(cc_test, ${tests_kind})" > bazel_tests_clang_tidy
 
-if [ "${all_targets}" = "true" ] || [[ -s bazel_buildables_cc ]] || [[ -s bazel_tests_cc ]]; then
-  touch bazel_cc_changed
+# Should we run doxygen?
+bazel_buildables_cc=$(${bazel_query} "kind(cc_binary, ${buildables_kind})")
+bazel_tests_cc=$(${bazel_query} "kind(cc_binary, ${buildables_kind})")
+if [ "${all_targets}" = "true" ] || [[ -z $bazel_buildables_cc ]] || [[ -z $bazel_tests_cc ]]; then
+  touch run_doxygen
 fi

commit 344cb9dad3f5493b73cb698cb245ee9efe505760

Fix doxygen run in jenkins
Summary:
The buildables stash rule only stashes files beginning with `bazel_`.
As a result `run_doxygen` never exists in the docs runtime stage even though
the build deps stage might create it.
We are also checking for empty cc targets instead of non empty to determine
whether we should run doxygen.
Note that this has been broken since D6922 :(

This fixes both problems.

Test Plan: This diff should cause doxygen to run.

Reviewers: zasgar, oazizi, michelle

Reviewed By: oazizi

Signed-off-by: Vihang Mehta <vihang@pixielabs.ai>

Differential Revision: https://phab.corp.pixielabs.ai/D12635

GitOrigin-RevId: e0af41751a5cb426e2990675fff1846997be95d3

Jenkinsfile
@@ -918,10 +918,10 @@ builders['Lint & Docs'] = {
       sh 'arc lint --trace'
     }
 
-    if (shFileExists('run_doxygen')) {
+    if (shFileExists('bazel_run_doxygen')) {
       def stashName = 'doxygen-docs'
       container('pxbuild') {
-        sh 'doxygen'
+        sh 'LD_LIBRARY_PATH="" doxygen'
       }
       container('gcloud') {
         stashOnGCS(stashName, 'docs/html')

ci/bazel_build_deps.sh
@@ -97,7 +97,7 @@ done
 
 targets=()
 function compute_targets() {
-  if [ "${all_targets}" = "true" ]; then
+  if [[ "${all_targets}" = "true" ]]; then
     targets=("//...")
     return 0
   fi
@@ -176,7 +176,7 @@ ${bazel_query} "${cc_tests} ${bpf_excludes}" > bazel_tests_gcc_opt 2>/dev/null
 ${bazel_query} "${cc_buildables} ${bpf_excludes} ${sanitizer_only}" > bazel_buildables_sanitizer 2>/dev/null
 ${bazel_query} "${cc_tests} ${bpf_excludes} ${sanitizer_only}" > bazel_tests_sanitizer 2>/dev/null
 
-if [ "${run_bpf_targets}" = "true" ]; then
+if [[ "${run_bpf_targets}" = "true" ]]; then
   # BPF.
   ${bazel_query} "${bpf_buildables}" > bazel_buildables_bpf 2>/dev/null
   ${bazel_query} "${bpf_tests}" > bazel_tests_bpf 2>/dev/null
@@ -204,6 +204,6 @@ ${bazel_query} "${go_tests} ${go_xcompile_excludes}" > bazel_tests_go_race 2>/de
 
 # Should we run doxygen?
 bazel_cc_touched=$(${bazel_query} "${cc_buildables} union ${cc_tests}" 2>/dev/null)
-if [ "${all_targets}" = "true" ] || [[ -z $bazel_cc_touched ]]; then
-  touch run_doxygen
+if [[ "${all_targets}" = "true" || -n $bazel_cc_touched ]]; then
+  touch bazel_run_doxygen
 fi
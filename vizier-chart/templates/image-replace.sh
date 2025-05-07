c56fc280c1f947a24bc865a59950e6907b1677e4
entlein <einentlein@gmail.com>
2025-05-07 18:36:29 +0200

    test if this is a feasible approach to overwrite the skaffold produced artefacts
    Signed-off-by: entlein <einentlein@gmail.com>

---
+++ vizier-chart/templates/image-replace.sh
@@ -0,0 +1,63 @@
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+00_secrets.yaml
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+01_nats.yaml
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+02_etcd.yaml
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+03_vizier_etcd.yaml
+
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+04_vizier_persistent.yaml
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+05_vizier_etcd_ap.yaml
+
+sed -i '' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-pem_image:0.14.15|ghcr.io/k8sstormcenter/vizier-pem_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-kelvin_image:0.14.15|ghcr.io/k8sstormcenter/vizier-kelvin_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-metadata_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-metadata_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-query_broker_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-query_broker_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cloud_connector_server_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cloud_connector_server_image:{{ .Values.imageTag }}|g' \
+-e 's|gcr.io-pixie-oss-pixie-prod-vizier-cert_provisioner_image:0.14.15|ghcr.io/k8sstormcenter/vizier-cert_provisioner_image:{{ .Values.imageTag }}|g' \
+06_vizier_persistent_ap.yaml

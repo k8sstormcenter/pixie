698cb0debd28e49e0f1702ec3291d3a7399b53d1
entlein <einentlein@gmail.com>
2025-05-12 11:30:35 +0200

    feature: adding helm installer
    Signed-off-by: entlein <einentlein@gmail.com>

--- vizier-chart/helm-install.sh
+++ vizier-chart/helm-install.sh
@@ -1,4 +1,5 @@
 
+#First you install pixie via px deploy, then run this script
 kubectl get secret -n pl -o json | jq -r '.items[] | select(.metadata.annotations | has("helm.sh/release-name") | not) | .metadata.name' | xargs -I {} kubectl annotate -n pl --overwrite secret {} meta.helm.sh/release-name=pixie meta.helm.sh/release-namespace=pl 
 kubectl get svc  -n pl -o json   | jq -r '.items[] | select(.metadata.annotations | has("helm.sh/release-name") | not) | .metadata.name' | xargs -I {} kubectl annotate -n pl --overwrite svc {} meta.helm.sh/release-name=pixie meta.helm.sh/release-namespace=pl 
 kubectl get sa -n pl -o json     | jq -r '.items[] | select(.metadata.annotations | has("helm.sh/release-name") | not) | .metadata.name' | xargs -I {} kubectl annotate -n pl sa --overwrite {} meta.helm.sh/release-name=pixie meta.helm.sh/release-namespace=pl 
@@ -27,5 +28,13 @@ kubectl get ds -n pl -o json     | jq '.items[] | .metadata | select(.labels."ap
 kubectl get deployment -n pl -o json     | jq '.items[] | .metadata | select(.labels."app.kubernetes.io/managed-by=Helm" | not) | .name' | xargs -I {} kubectl label -n pl deployment     --overwrite {} app.kubernetes.io/managed-by=Helm
 kubectl get statefulset -n pl -o json     | jq '.items[] | .metadata | select(.labels."app.kubernetes.io/managed-by=Helm" | not) | .name' | xargs -I {} kubectl label -n pl statefulset     --overwrite {} app.kubernetes.io/managed-by=Helm
 
+keyid=f60a3c55-91fe-4dbc-b984-bf6ed4fdc323  
+key=$(px api-key get $keyid)
+if [ ! -f myvalues.yaml ]; then
+  echo "Error: myvalues.yaml not found"
+  exit 1
+fi
+
+helm upgrade --install pixie . --namespace pl --create-namespace --values myvalues.yaml
 
 

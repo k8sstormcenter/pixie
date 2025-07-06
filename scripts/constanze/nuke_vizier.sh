a0a35c03e98d1bc3c0896aca042afc9b230d63bd
entlein <einentlein@gmail.com>
2025-07-07 01:33:01 +0200

    debug scripts with results- conclusion: L8 error schema was incorrect
    Signed-off-by: entlein <einentlein@gmail.com>

--- scripts/constanze/nuke_vizier.sh
+++ scripts/constanze/nuke_vizier.sh
@@ -2,3 +2,5 @@ kubectl rollout restart -n pl statefulset vizier-metadata
 kubectl delete -n pl pvc  metadata-pv-claim
 pvname=$(kubectl get pv | cut -d " " -f 1 |grep pvc)
 kubectl delete pv $pvname
+
+#better not to use this script - I m not sure what cause one of the GKE nodes to become unschedulable but I had to delete the node and wait for a new one
